eb363653c1b54fcd5d92050c6c80be794c5ddc40
Fran <valiente.fco@gmail.com>
2023-05-21 13:13:39 +0200

    login problem2

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -1,7 +1,7 @@
 import {connect, ConnectOptions} from 'mongoose';
 
 export const dbConnect = () => {
-    connect('mongodb+srv://FranBrave:V4hXpp6BxuOofNG6@franbrave.qzw1i.mongodb.net/DeliFood?retryWrites=true&w=majority', {
+    connect(process.env.MONGO_URI! , {
         useNewUrlParser: true,
         useUnifiedTopology: true
     } as ConnectOptions).then(
